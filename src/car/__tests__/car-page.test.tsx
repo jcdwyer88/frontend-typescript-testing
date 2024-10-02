77a42dcfe4bccc7e5d7ed25b9a45528d30f6267b
Josh Farrington <joshua.l.farrington.civ@army.mil>
2024-10-02 16:35:38 -0500

    Step 6: display list of cars

--- src/car/__tests__/car-page.test.tsx
+++ src/car/__tests__/car-page.test.tsx
@@ -4,15 +4,24 @@ import {expect} from "vitest";
 import * as carService from '../car-service.ts'
 
 describe('Car Page', () => {
-  it('should display car header', () => {
+  it('should display car header', async () => {
     vi.spyOn(carService, 'fetchCars').mockResolvedValue([])
     render(<CarPage />)
-    expect(screen.getByRole('heading', {name: 'Cars'})).toBeVisible()
+    expect(await screen.findByRole('heading', {name: 'Cars'})).toBeVisible()
   });
 
-  it('should call car service', () => {
+  it('should call car service', async () => {
     const mockFetchCars = vi.spyOn(carService, 'fetchCars').mockResolvedValue([]);
     render(<CarPage />)
     expect(mockFetchCars).toHaveBeenCalledTimes(1)
+    expect(await screen.findByRole('heading', {name: 'Cars'})).toBeVisible()
+  });
+
+  it('should display a list of cars', async () => {
+    const car = { id: 1, make: 'Tesla', model: 'Model Y'};
+    vi.spyOn(carService, 'fetchCars').mockResolvedValue([car])
+    render(<CarPage />)
+    expect(await screen.findByRole('heading', {name: car.make})).toBeVisible()
+    expect(screen.getAllByRole('listitem')).toHaveLength(1)
   });
 });
--- src/car/car-page.tsx
+++ src/car/car-page.tsx
@@ -1,14 +1,21 @@
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {fetchCars} from "./car-service.ts";
+import {Car} from "../types.ts";
+import CarCard from "./car-card.tsx";
 
 const CarPage = () => {
+  const [cars, setCars] = useState<Car[]>([])
+
   useEffect(() => {
-    fetchCars()
+    fetchCars().then(setCars)
   }, []);
 
   return (
     <div>
       <h2>Cars</h2>
+      <ul>
+        {cars.map(car => (<CarCard key={car.id} car={car}/>))}
+      </ul>
     </div>
   );
 };
